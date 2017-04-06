2e29319617a400c2419187fdbdfc9804f6e443ab
intan6706150137 <intankhair02@gmail.com>
2017-04-06 09:40:03 +0700

    Update JavaApplication2.java

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -23,7 +23,7 @@ public class JavaApplication2 {
         int n = 12/2;
         for(int i=1;i<=n;i++){
             for(int k=n;k>i;k--){
-                System.out.print(" ");
+                System.out.print("0");
             }
             for(int j=1;j<=l;j++){
                 System.out.print("*");
@@ -34,7 +34,7 @@ public class JavaApplication2 {
             l -= 4;
             for(int i=(n-1);i>=1;i--){
                 for(int k=i;k<=(n-1);k++){
-                    System.out.print(" ");
+                    System.out.print("0");
             }
             for(int j=l;j>=1;j--){
                 System.out.print("*");
